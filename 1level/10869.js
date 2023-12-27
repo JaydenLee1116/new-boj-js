f4303abe65d2bc5e7a98f45eff9604d0dc8ea172
JaydenLee1116 <vv55adss@gmail.com>
2023-12-28 08:00:34 +0900

    🌱 grow: level1 ~ 10430까지
    - 자료구조, 알고리즘에 대해 근본적인 공부도 같이 하자

---
+++ 1level/10869.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '../input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+const [A, B] = input[0].split(' ').map((v) => +v);
+
+console.log(A + B);
+console.log(A - B);
+console.log(A * B);
+console.log(Math.floor(A / B));
+console.log(A % B);
---
+++ 1level/18108.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '../input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+const y = +input[0];
+
+console.log(y - 543);
